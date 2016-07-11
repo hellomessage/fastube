12667edc0bec9498433b6b1abd40003153819c60
message0316 <message0316@gmail.com>
2016-07-11 14:23:16 +0900

    init LogoutView: perform logout and redirect to login page

--- fastube/fastube/urls.py
+++ fastube/fastube/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from fastube.views import *
 from users.views import *
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view(), name="home"),
     url(r'^signup/', SignupView.as_view(), name="signup"),
     url(r'^login/', LoginView.as_view(), name="login"),
+    url(r'^logout/', LogoutView.as_view(), name="logout"),
 ]
--- fastube/users/views/login.py
+++ fastube/users/views/login.py
@@ -17,7 +17,8 @@ class LoginView(View):
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        next_url = request.POST.get("next") or reverse("login")     # FIXME: redirect to home
+        next_url = request.POST.get("next_url") or reverse("home")
+
         user = authenticate(
             username=username,
             password=password,
