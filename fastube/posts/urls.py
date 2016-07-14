48610794323e4529af8bf137f5fc401b1bf0b9d1
message0316 <message0316@gmail.com>
2016-07-14 10:29:49 +0900

    init PostCreateConfirmView: create youtube_original, youtube_embed_url from vidio_id

--- fastube/posts/urls.py
+++ fastube/posts/urls.py
@@ -5,4 +5,5 @@ from posts.views import *
 
 urlpatterns = [
     url(r'^new/$', PostCreateFormView.as_view(), name="create"),
+    url(r'^confirm/$', PostCreateConfirmView.as_view(), name="confirm"),
 ]
--- fastube/posts/views/create.py
+++ fastube/posts/views/create.py
@@ -1,5 +1,8 @@
+from django.core.urlresolvers import reverse
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from posts.utils import youtube
 
 
 class PostCreateFormView(View):
@@ -12,5 +15,25 @@ class PostCreateFormView(View):
         )
 
         
-class PosrtCreateConfirmView(View):
-    pass
+class PostCreateConfirmView(View):
+
+    def get(self, request, *args, **kwargs):
+        return redirect(reverse("posts:create"))
+
+    def post(self, request, *args, **kwargs):
+        video_id = request.POST.get("video_id")
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+
+        return render(
+            request,
+            "posts/confirm.html",
+            context={
+                "video_id": video_id,
+                "title": title,
+                "content": content,
+
+                "youtube_original_url": youtube.get_youtube_original_url(video_id),
+                "youtube_embed_url": youtube.get_youtube_embed_url(video_id),
+            },
+        )
